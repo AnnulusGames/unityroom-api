e2f11fdfd5c8d802b4eff225d20a5b646e908ee4
AnnulusGames <annulusgames@gmail.com>
2023-11-12 09:39:00 +0900

    HmacのUTF8Encodingをキャッシュ

--- Assets/Unityroom/Runtime/Internal/Hmac.cs
+++ Assets/Unityroom/Runtime/Internal/Hmac.cs
@@ -1,6 +1,7 @@
-using System.Runtime.CompilerServices;
 using System;
 using System.Text;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 [assembly: InternalsVisibleTo("Unityroom.Tests.Editor")]
 
@@ -14,16 +15,18 @@ namespace Unityroom.Internal
         /// <param name="dataText">計算対象文字列</param>
         /// <param name="base64AuthenticationKey">HMACSHA256に用いるキー（base64）</param>
         /// <returns>HMAC SHA-256</returns>
-        internal static string GetHmacSha256(string dataText, string base64AuthenticationKey)
+        public static string GetHmacSha256(string dataText, string base64AuthenticationKey)
         {
-            var dataBytes = new UTF8Encoding(false).GetBytes(dataText); // BOM無しUTF-8のbyte配列に変換
+            var dataBytes = encoding.GetBytes(dataText); // BOM無しUTF-8のbyte配列に変換
             var keyBytes = Convert.FromBase64String(base64AuthenticationKey); // base64の認証用キーをデコードしてbyte配列に変換
-            var sha256 = new System.Security.Cryptography.HMACSHA256(keyBytes); // ハッシュ関数はSHA256を用いる
+            var sha256 = new HMACSHA256(keyBytes); // ハッシュ関数はSHA256を用いる
             var hmacBytes = sha256.ComputeHash(dataBytes); // ハッシュ値を計算
             var hmacText = BitConverter.ToString(hmacBytes)
                 .ToLower()
                 .Replace("-", ""); // byte型配列を16進数の文字列に変換
             return hmacText;
         }
+
+        static readonly UTF8Encoding encoding = new(false);
     }
 }
